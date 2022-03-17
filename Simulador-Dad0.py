46f7e734a4638f9fd5f2159749068540d8535703
WesleyEnrique <wesleyarauoj18@gmail.com>
2022-03-16 22:48:54 -0300

    first commit

---
+++ Simulador-Dad0.py
@@ -0,0 +1,29 @@
+#Simulador de dado 
+#Simular o uso de um dado, grando um valor de 1 até 6 
+from logging import exception
+import random
+
+class SimuladorDeDado:
+    def __init__(self) -> None:
+        self.valor_minimo = 1
+        self.valor_maximo = 6 
+        self.mensagem = 'Você gostaria de gerar um novo valor para o dado ? '
+
+    def Iniciar(self):
+        resposta = input(self.mensagem)
+        try:
+            if resposta == 'sim' or resposta == 's':
+                self.GerarValorDoDado()
+            elif resposta == 'não' or resposta == 'n':
+                print("Agradecemos sua participação")
+            else:
+                print('Favor digitar sim ou não')
+        exception:
+            print('Ocorreu um erro ao receber a resposta')
+
+        
+    def GerarValorDoDado(self):
+        print(random.randint(self.valor_minimo,self.valor_maximo))
+
+simulador = SimuladorDeDado()
+simulador.Iniciar()
